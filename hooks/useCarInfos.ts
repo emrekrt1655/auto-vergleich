9e756e478677eb0eaabe20b2e1551641be603b43
Emre <emrekurtt1655@gmail.com>
2025-11-22 01:02:37 +0100

    feat: add car brands and models data files and integrate initial data into useCarBrands hook

--- hooks/useCarInfos.ts
+++ hooks/useCarInfos.ts
@@ -1,11 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCarBrands, getCarModels } from "@/services/carApiService";
 import type { CarBrand, CarModel } from "@/types/Car";
+import carBrands from "@/data/carBrands.json" assert { type: "json" };
 
 export const useCarBrands = () => {
   return useQuery<CarBrand[]>({
     queryKey: ["carBrands"],
     queryFn: () => getCarBrands(),
+    initialData: carBrands,
     staleTime: 1000 * 60 * 60,
     refetchOnWindowFocus: false,
   });
